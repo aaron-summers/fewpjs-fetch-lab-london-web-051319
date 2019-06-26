0976e9772d7b49d2ed96a3c802c9f2cfaa48c3fe
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-06-26 12:47:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ function fetchBooks() {
     .then(booksArray => renderBooks(booksArray));
 }
 
-// function renderBooks(json) {
-//   const main = document.querySelector('main')
-//   json.forEach(book => {
-//     const h2 = document.createElement('h2')
-//     h2.innerHTML = `<h2>${book.name}</h2>`
-//     main.appendChild(h2)
-//   })
-// }
+function renderBooks(json) {
+  const main = document.querySelector('main')
+  json.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
+  })
+}
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
